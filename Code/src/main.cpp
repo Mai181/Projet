2065dc2bb32539629a5eaa2ef99bc9dd977ea1e7
Mai181 <maina.clermont@gmail.com>
2024-10-03 12:00:44 -0400

    m

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,6 +1,14 @@
+/********
+    Fichier: main.cpp
+    Auteurs: P-19
+    Date: 26 septembre 2024
+    Description: code principal des robots
+********/
+
 #include <Arduino.h>
 #include <librobus.h>
 #include <Rotations.h>
+#include <avancement.h>
 
 void setup() {
 	BoardInit();
