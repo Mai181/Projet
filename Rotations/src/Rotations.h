dfb30083e647bb499fb1a16424c66edcc0029e77
Mai181 <143757239+Mai181@users.noreply.github.com>
2024-10-03 10:02:39 -0400

    Add files via upload

---
+++ Rotations/src/Rotations.h
@@ -0,0 +1,11 @@
+/*
+    Fichier: Rotations
+    Auteurs: Maina Clermont 
+    Date: 26 septembre 2024
+    Description: rotation des robots
+*/
+
+// Fonction pour tourner à gauche
+void rotationGauche(int a);
+// Fonction pour tourner a droite
+void rotationDroite(int a);
