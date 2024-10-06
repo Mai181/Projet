b1835e9ea1be5b6607c6bf56657900fc0c8238fc
tfaucher04 <tfaucher04@gmail.com>
2024-10-06 16:59:37 -0400

    rot
    modification rotation

--- Code/src/Rotations.cpp
+++ Code/src/Rotations.cpp
@@ -12,8 +12,8 @@ float cirCerRot = 114.98;  // circonferenceCercleRotation
 float cirRoue = 23.94;  // circonferenceRoue
 int tour = 360;
 int pulseTourRoue = 3200; 
-float vn = -0.5;  // vitesse négative du moteur 
-float vp = 0.5;  // vitesse positive du moteur
+float vn = -0.25;  // vitesse négative du moteur 
+float vp = 0.25;  // vitesse positive du moteur
 // vitesse a tester avec le robot
 
 // Fonction qui fait tourner le robot a gauche
@@ -23,7 +23,10 @@ void rotationGauche(int a) {
     while(ENCODER_Read(LEFT) != pulse) {
         MOTOR_SetSpeed(LEFT, vn);
         MOTOR_SetSpeed(RIGHT, vp); 
+        
     }
+    MOTOR_SetSpeed(LEFT, 0);
+    MOTOR_SetSpeed(RIGHT, 0);
     //manque quelle que chose pour arreter de tourner ( vitesse = 0 )
     //idealement faudrait des rampe d'acceleration et deceleration pour combatre l'inertie du robot en rotation
     /*si manque de pression apres les tests il pourrait avoir un reajustement de la position selon les valeur attendu 
@@ -37,6 +40,9 @@ void rotationDroite(int a) {
     while(ENCODER_Read(LEFT) != pulse) {
         MOTOR_SetSpeed(LEFT, vp);
         MOTOR_SetSpeed(RIGHT, vn); 
+
     }
+    MOTOR_SetSpeed(LEFT, 0);
+    MOTOR_SetSpeed(RIGHT, 0);
     //meme chose qu'en haut
 }
