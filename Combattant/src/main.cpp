4c57cc529267fe522f93ee975d4eb1fc584ca3e8
godrofranck <godrofrancky@gmail.com>
2024-10-26 13:19:22 -0400

    sifflet

--- Combattant/src/main.cpp
+++ Combattant/src/main.cpp
@@ -212,6 +212,26 @@ float limiterRot(float valeur, float minVal, float maxVal) {
     return valeur;
 }
 
+/** Fonction pour détecter le signal de départ de 5kHz */
+bool detectionSifflet(){
+
+    bool sifflet = false; //par défaut, il n'y a pas de sifflet
+
+    int bruitAmbiant = analogRead(A14); //l'information lue à la pin 10 se trouve entre 0 et 1023
+    float voltageBruitAmbiant = bruitAmbiant * (5.0 / 1023.0); //produit croisé pour ramener sur 5V (l'alim du circuit)
+//    Serial.println(voltageBruitAmbiant);
+
+    int cinqkH = analogRead(A15);
+    float voltage5kH = cinqkH * (5.0 / 1023.0);
+//    Serial.println(voltage5kH);
+
+    if (voltage5kH > voltageBruitAmbiant){
+        sifflet = true;
+    }
+
+    return sifflet;
+}
+
                             /*************************Fonctions pour les déplacements**************************/
                             
 /** Fonction pour limiter une valeur dans une plage donnée
