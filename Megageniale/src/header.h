e5cfba41611362cd5257afa5708974061504ec5a
Mai181 <maina.clermont@gmail.com>
2024-11-11 23:50:06 -0500

    creation file Megageniale

---
+++ Megageniale/src/header.h
@@ -0,0 +1,9 @@
+/*    
+*   Fichier: header.h
+*   Auteurs: P-19
+*   Date: 11 novembre 2024
+*   Description: header des fonctions du main 
+*                (les fonctions sont implémentées 
+*                dans des fichiers différents du main)
+*/
+
---
+++ Megageniale/src/main.cpp
@@ -0,0 +1,10 @@
+/*    
+*   Fichier: main.cpp
+*   Auteurs: P-19
+*   Date: 11 novembre 2024
+*   Description: code principal du robot
+*/
+
+#include <Arduino.h>
+#include <librobus.h>
+#include "header.h"
