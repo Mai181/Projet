efa7409ea6acb4c29fa37954c372b8103a283e12
Mai181 <maina.clermont@gmail.com>
2024-11-11 23:54:31 -0500

    debut main

--- Megageniale/src/main.cpp
+++ Megageniale/src/main.cpp
@@ -2,9 +2,13 @@
 *   Fichier: main.cpp
 *   Auteurs: P-19
 *   Date: 11 novembre 2024
-*   Description: code principal du robot
+*   Description: code PRINCIPAL du robot
 */
 
 #include <Arduino.h>
 #include <librobus.h>
 #include "header.h"
+
+int main(){
+    return 0;
+}
